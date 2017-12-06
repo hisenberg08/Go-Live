389571e00ce94defd32b44515466d0cf4a3a8c86
Rohan Tigadi <Rohan Tigadi@192.168.0.9>
2017-12-05 22:02:01 -0500

    partial search

--- src/main/java/com/golive/launch/repo/SearchRepo.java
+++ src/main/java/com/golive/launch/repo/SearchRepo.java
@@ -15,24 +15,59 @@ public class SearchRepo {
 	@Autowired
 	private JdbcTemplate jdbctempplate;
 
-	public Map<Integer, List<String>> searchAll(String searchFor) {
+	public List<Map<Integer, List<String>>> searchAll(String searchFor) {
+		List<Map<Integer, List<String>>> results = new ArrayList<Map<Integer, List<String>>>();
 
-		String query = "select trackid, tracktitle, trackduration, trackreleasedate from track where tracktitle LIKE ?";
-		Map<Integer, List<String>> resultMap = new HashMap<Integer, List<String>>();
+		String trackQuery = "select trackid, tracktitle, trackduration, trackreleasedate from track where tracktitle LIKE ?";
+		String artistQuery = "select artistid, artistname, artistdesc from artist where artistname LIKE ?";
+		String albumQuery = "select albumid, albumtitle, albumreleasedate from album where albumtitle LIKE ?";
 
-		List<Map<String, Object>> res = jdbctempplate.queryForList(query, searchFor);
+		Map<Integer, List<String>> trackResultMap = new HashMap<Integer, List<String>>();
+		Map<Integer, List<String>> artistResultMap = new HashMap<Integer, List<String>>();
+		Map<Integer, List<String>> albumResultMap = new HashMap<Integer, List<String>>();
+
+		List<Map<String, Object>> trackResults = jdbctempplate.queryForList(trackQuery, searchFor);
+		List<Map<String, Object>> artistResults = jdbctempplate.queryForList(artistQuery, searchFor);
+		List<Map<String, Object>> albumResults = jdbctempplate.queryForList(albumQuery, searchFor);
+
 		int i = 1;
-
-		for (Map<String, Object> result : res) {
-			List<String> record = new ArrayList<String>();
-			record.add(result.get("trackid").toString());
-			record.add(result.get("tracktitle").toString());
-			record.add(result.get("trackduration").toString());
-			record.add(result.get("trackreleasedate").toString());
-			resultMap.put(i, record);
+		// mapping result from track fetch query into HashMap
+		for (Map<String, Object> trackResult : trackResults) {
+			List<String> trackRecord = new ArrayList<String>();
+			trackRecord.add(trackResult.get("trackid").toString());
+			trackRecord.add(trackResult.get("tracktitle").toString());
+			trackRecord.add(trackResult.get("trackduration").toString());
+			trackRecord.add(trackResult.get("trackreleasedate").toString());
+			trackResultMap.put(i, trackRecord);
 			i++;
 		}
-		return resultMap;
+
+		i = 1;
+		// mapping result from artist fetch query into HashMap
+		for (Map<String, Object> artistResult : artistResults) {
+			List<String> artistRecord = new ArrayList<String>();
+			artistRecord.add(artistResult.get("artistid").toString());
+			artistRecord.add(artistResult.get("artistname").toString());
+			artistRecord.add(artistResult.get("artistdesc").toString());
+			artistResultMap.put(i, artistRecord);
+			i++;
+		}
+
+		i = 1;
+		// mapping result from artist fetch query into HashMap
+		for (Map<String, Object> albumResult : albumResults) {
+			List<String> albumRecord = new ArrayList<String>();
+			albumRecord.add(albumResult.get("albumid").toString());
+			albumRecord.add(albumResult.get("albumtitle").toString());
+			albumRecord.add(albumResult.get("albumreleasedate").toString());
+			albumResultMap.put(i, albumRecord);
+			i++;
+		}
+
+		results.add(trackResultMap);
+		results.add(artistResultMap);
+		results.add(albumResultMap);
+		return results;
 	}
 
 }
